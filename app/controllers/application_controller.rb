083765e4dfd7928a5fe4733dc8d37726e9c7b618
Luis Herrera <herrera.lrh@gmail.com>
2020-11-22 03:25:56 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,4 +20,13 @@ class ApplicationController < Sinatra::Base
     erb :error
   end
 
+  get '/account' do
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+      erb :account
+    else
+      erb :error
+    end
+  end
+  
 end
